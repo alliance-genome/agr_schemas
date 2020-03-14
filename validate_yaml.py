dd0165f9eb8b28f37353da50822fd11e506e1cdf
Adam Wright <adam.j.wright82@gmail.com>
2020-03-13 22:03:44 -0400

    made it so that yaml_validator will validate all yaml files or just specified file

--- validate_yaml.py
+++ validate_yaml.py
@@ -1,18 +1,28 @@
-#!/usr/bin/env python 
-import yaml 
+#!/usr/bin/env python
+import yaml
 import os
+import sys
+import glob
+import base64
 import argparse
 
-parser = argparse.ArgumentParser(description='This is a simple validator for resourceDescriptors.yaml.')
-
-parser.add_argument('-d','--data', help='resourceDescriptor.yaml',required=True)
+parser = argparse.ArgumentParser(description='This is a simple validator for all yaml files in repo')
+parser.add_argument('file',
+                    nargs='?',
+                    type=argparse.FileType('r'),
+                    #dest="file",
+                    default=sys.stdin,
+                    help='Use this flag to specify a specific file')
 args = parser.parse_args()
 
-data_file_name = args.data
-with open(data_file_name) as data_file:
-    try: 
-        data = yaml.load(data_file)
-        #print(yaml.dump(data))
-        print ("successfully validated " + args.data)
-    except yaml.YAMLError as exc:
-        print (exc)
+if not args.file.name == '<stdin>':
+    yaml_filepaths = [args.file.name]
+else:
+    yaml_filepaths = [y for x in os.walk(os.getcwd()) for y in glob.glob(os.path.join(x[0], '*.yaml'))]
+
+for yaml_filepath in yaml_filepaths:
+    print("Validating " + yaml_filepath)
+    with open(yaml_filepath, "r") as file_stream:
+        yaml_data = file_stream.readlines()
+        data = yaml.safe_load("".join(yaml_data))
+        print ("Valid YAML")
